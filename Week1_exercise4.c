bbcbe2f3db2dca54af6e3a2448c5520ede6a3e3a
Solexi <sophia.ezeh@pau.edu.ng>
2021-11-08 16:06:54 +0100

    hi

---
+++ Week1_exercise4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{	
+	char myInitial = 'S';
+	char myName[] = "Sophia Ezeh";
+	printf("Your name is = %s\n", myName);
+	printf("The initial of your firstname is = %c\n", myInitial);
+	return 0;
+}
---
+++ Week1_exercise5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{	
+	int StudentAge;
+	printf("Enter age of student: ");
+	scanf("%d", &StudentAge);
+	printf("Student age = ", StudentAge);
+	return 0;
+}
